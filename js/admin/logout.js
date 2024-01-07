b412752c468eb912af0289ed0d9293e79524ed68
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-01-07 18:49:38 +0700

    update modal logout

--- js/admin/logout.js
+++ js/admin/logout.js
@@ -1,5 +1,18 @@
 let account = JSON.parse(localStorage.getItem("adminLogin")) || {};
+
+const btnModalLogout = document.querySelector("#btnModalLogout");
+const modalLogOut = document.querySelector("#modalLogOut");
 const btnLogout = document.querySelector("#btnLogout");
+btnModalLogout.addEventListener("click", (e) => {
+  e.stopPropagation();
+  modalLogOut.style.display = "flex";
+});
+window.addEventListener("click", () => {
+  if (modalLogOut.style.display === "flex") {
+    modalLogOut.style.display = "none";
+  }
+});
+
 btnLogout.addEventListener("click", () => {
   localStorage.removeItem("adminLogin");
 
